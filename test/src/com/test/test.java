dde83e998e974ce8dea0d9f9044fbf05721d7c0b
Administrator <Administrator@QH-20190826ZHQZ>
2020-06-19 16:21:54 +0800

    第二次提交

--- test/src/com/test/test.java
+++ test/src/com/test/test.java
@@ -3,5 +3,9 @@ package com.test;
 public class test {
 public static void main(String[] args) {
 	System.out.println("ssss");
+	System.out.println("aaaa");
+	System.out.println("bbbb");
+	System.out.println("cccc");
+	System.out.println("dddd");
 }
 }
